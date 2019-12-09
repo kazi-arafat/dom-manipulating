6df39b6aecce49b772feca67ad3a860993359529
Kazi-arafat <arafat.kazi2448@gmail.com>
2019-12-10 00:48:35 +0530

    ain js modified

--- Learning.md
+++ Learning.md
@@ -1,15 +1,23 @@
-# DOM Manipulation Using Vanila JavaScript
+# DOM Manipulation Using Vanilla JavaScript
+--------------------------------------------
 
-1. Documnet object model, a structured representation of web page.
-2. Tree of nodes/elements created by the browser.
-3. Object oriented reprsentation. Each node has its own properties and methods.
+* Documnet object model, a structured representation of web page.
+* Tree of nodes/elements created by the browser.
+* Object oriented reprsentation. Each node has its own properties and methods.
 
    ![dom_image](images/dom.png)
 
-4. Browser provides top level object named **window** object, which is the browser itself.
+* Browser provides top level object named **window** object, which is the browser itself.
 
    Then within **window**, we have **Documnet**(basically core of the DOM) object which provides **Root element.**
 
    Within **Root element** we have two children/nodes named **head** and **body**.
 
    and these element have all the other tags as the child 
+--------------------------------------------------------------------
+
+* ```javascript
+  console.dir(document) 
+  /* Prints all the relevant info about document in consle */
+  ```
+* With querySelector() we can use css query selector.  
--- main.js
+++ main.js
@@ -1,1 +1,57 @@
 // DOM Manipulation using JS
+// console.dir(document) 
+/* Prints all the relevant info properties and methods
+ about document in consle */
+
+// Examine the documnet object
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);  // prints HTMLAllCollection
+// console.log(document.forms)
+// collection of all the forms in document
+
+// Single Selector
+
+let headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+// console.log(headerTitle.innerHTML);
+// headerTitle.textContent = "Hello
+
+var items = document.getElementsByClassName('list-group-item');
+
+for(let i = 0; i < items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4f4';
+    // items[i].style.backgroundColor = 'black'
+}
+
+// get elements by tag name
+
+// Qery Selector
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 5px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'hello World';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "Send";
+
+var firstItem = document.querySelector('.list-group-item:first-child');
+firstItem.style.color = "blue";
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = "green";
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = "coral";
+
+
+// Qery Selector All
+var titles = document.querySelectorAll('.title')
+console.log(titles)
+
+titles[0].textContent = "Hello"
